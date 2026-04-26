91874176a9f92c6ff05f7e1c8131e3403a9c2377
mmanov <mmanov@adsterra.com>
2026-04-26 15:15:00 +0300

    fix error

--- internal/repositories/currency_repository.go
+++ internal/repositories/currency_repository.go
@@ -110,7 +110,7 @@ func (r *CurrencyRepository) GetCurrencyById(id int) (domain.Currency, error) {
 
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return domain.Currency{}, sql.ErrNoRows
+			return domain.Currency{}, domain.ErrCurrencyNotFound
 		}
 		return domain.Currency{}, err
 	}
